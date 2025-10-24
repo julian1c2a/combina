6969d06b8d192f9be7e6431e8068a8fdd1232167
Julián Calderón Almendros <earendil.elrond.arwen@gmail.com>
2025-10-24 20:54:55 +0200

    Add support for larger integer types in factorial calculations and update CMake configuration

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <int128_helpers.hpp>
+#include <factoriales.hpp>
 #include <test_helpers.hpp>
 #include <test_advanced.hpp> // Incluir la nueva cabecera de pruebas
 
@@ -7,20 +8,29 @@ using namespace integer_literals;
 
 int main() {
     run_tests(); // Ejecutar las pruebas básicas primero
-    run_advanced_tests(); // Ejecutar las nuevas pruebas avanzadas
+    // run_advanced_tests(); // Ejecutar las nuevas pruebas avanzadas
 
-    std::cout << "Introduce un numero grande (de hasta 128 bits):" << std::endl;
-    uint128_t my_number;
-    std::cin >> my_number;
+    // std::cout << "Introduce un numero grande (de hasta 128 bits):" << std::endl;
+    // uint128_t my_number;
+    // std::cin >> my_number;
+    //
+    // if (std::cin.good()) {
+    //     std::cout << "Has introducido: " << my_number << std::endl;
+    //     uint128_t max_val = "340282366920938463463374607431768211455"_u128;
+    //     std::cout << "El valor maximo para uint128_t es: " << max_val << std::endl;
+    //     std::cout << "El numero por dos es: " << (my_number * 2) << std::endl;
+    // } else {
+    //     std::cout << "Entrada invalida." << std::endl;
+    // }
 
-    if (std::cin.good()) {
-        std::cout << "Has introducido: " << my_number << std::endl;
-        uint128_t max_val = "340282366920938463463374607431768211455"_u128;
-        std::cout << "El valor maximo para uint128_t es: " << max_val << std::endl;
-        std::cout << "El numero por dos es: " << (my_number * 2) << std::endl;
-    } else {
-        std::cout << "Entrada invalida." << std::endl;
-    }
+    const int128_t n_prev{33};
+    const int128_t n_next{34};
+
+    std::cout << std::endl;
+    std::cout << std::endl;
+
+    std::cout << n_prev << "! == " << factorial(n_prev) << std::endl;
+    std::cout << n_next << "! == " << factorial(n_next) << std::endl;
 
     return 0;
 }
