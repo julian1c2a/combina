1ded639cc1d4436cd7b28cb43ec05bacedda5a65
Julián Calderón Almendros <earendil.elrond.arwen@gmail.com>
2025-10-27 23:25:48 +0100

    Enhance factorial type traits with comprehensive error checks for negative inputs and overflow conditions

--- inc/type_traits_of_factorials.hpp
+++ inc/type_traits_of_factorials.hpp
@@ -123,7 +123,8 @@ namespace implementation_details {
     // --- Implementación interna ---
     // Recibe la secuencia de índices (0, 1, 2, ..., N-1) como un paquete de parámetros.
     template<typename T, size_t N, typename Func, size_t... Is>
-    constexpr std::array<T, N> make_constexpr_array_impl(Func &&f, index_sequence<Is...>) {
+    constexpr std::array<T, N> make_constexpr_array_impl(
+        Func &&f, index_sequence<Is...>) {
         // Usa una expansión de paquete para llamar a f(0), f(1), f(2), ...
         // y usar esos valores para inicializar el array.
         return {f(Is)...};
@@ -147,15 +148,115 @@ namespace implementation_details {
         );
     }
 
+    enum class fact_sign_info { negative, other };
+
+    enum class fact_over_info { overflow, other };
+
+    enum class fact_type_info { signed_int, unsigned_int };
+
+    enum class fact_capacity_info { finite, infty };
+
+    enum class fact_sz_in_table { in, out };
+
+    static constexpr size_t max_sz_fact_table{get_factorial_limit<uint1024_t>() + 1};
+
+    template<IsFactorialSupportedInteger T, T k>
+    constexpr fact_sign_info check_negative() {
+        if constexpr ((is_signed_v<T>) && (k < 0))
+            return fact_sign_info::negative;
+        else return fact_sign_info::other;
+    }
+
+    template<IsFactorialSupportedInteger T>
+    constexpr fact_sign_info check_negative_rt(T k) {
+        if ((is_signed_v<T>) && (k < 0))
+            return fact_sign_info::negative;
+        else return fact_sign_info::other;
+    }
+
+    template<IsFactorialSupportedInteger T, T k>
+    constexpr fact_over_info check_overflow() {
+        static constexpr size_t limit{get_factorial_limit<T>()};
+        if constexpr (limit != 0) {
+            if constexpr (k > limit) {
+                return fact_over_info::overflow;
+            } else {
+                return fact_over_info::other;
+            }
+        } else {
+            return fact_over_info::other;
+        }
+    }
+
+    template<IsFactorialSupportedInteger T>
+    constexpr fact_over_info check_overflow_rt(T k) {
+        static constexpr size_t limit{get_factorial_limit<T>()};
+        if constexpr (limit != 0) {
+            if (k > limit) {
+                return fact_over_info::overflow;
+            } else {
+                return fact_over_info::other;
+            }
+        } else {
+            return fact_over_info::other;
+        }
+    }
+
+    template<IsFactorialSupportedInteger T, T k>
+    constexpr fact_sz_in_table check_sz_in_table() {
+        if constexpr (k <= max_sz_fact_table) {
+            return fact_sz_in_table::in;
+        } else {
+            return fact_sz_in_table::out;
+        }
+    }
+
+    template<IsFactorialSupportedInteger T>
+    constexpr fact_sz_in_table check_sz_in_table_rt(T k) {
+        if (k <= max_sz_fact_table) {
+            return fact_sz_in_table::in;
+        } else {
+            return fact_sz_in_table::out;
+        }
+    }
+
+    template<IsFactorialSupportedInteger T>
+    constexpr fact_type_info sign_type_info() {
+        if constexpr (is_signed_v<T>) {
+            return fact_type_info::signed_int;
+        } else {
+            return fact_type_info::unsigned_int;
+        }
+    }
+
+    template<IsFactorialSupportedInteger T>
+    constexpr fact_capacity_info capacity_type_info() {
+        if constexpr (IsInftyInteger<T>) {
+            return fact_capacity_info::infty;
+        } else {
+            return fact_capacity_info::finite;
+        }
+    }
+
     template<IsFactorialSupportedInteger T, T k>
     constexpr T factorial_ct() {
         using std::numeric_limits;
-        constexpr size_t limit = get_factorial_limit<T>();
-        if constexpr ((is_signed_v<T>) && (k < 0)) {
+
+        static constexpr size_t limit{get_factorial_limit<T>()};
+
+        static constexpr bool is_negative{
+            check_negative<T, k>() == fact_sign_info::negative
+        };
+
+        static constexpr bool is_overflow{
+            check_overflow<T, k>() == fact_over_info::overflow
+        };
+
+        if constexpr (is_negative) {
             // Si el tipo entero es signado
             // y k es negativo
             return static_cast<T>(-1); // Devuelve -1 para números negativos
-        } else if constexpr ((limit != 0) && (k > limit)) {
+        } else if constexpr (is_overflow) {
             // Si el tipo es de longitud finita
             // y k es mayor que el límite
             return static_cast<T>(0); // Devuelve 0 para overflow
@@ -164,12 +265,16 @@ namespace implementation_details {
             for (size_t i{2}; i <= k; ++i) {
                 // Comprobación de overflow antes de multiplicar
                 if constexpr (limit != 0) {
-                    if (res > numeric_limits<T>::max() / static_cast<T>(i)) {
+                    const T division_check{
+                        numeric_limits<T>::max() / static_cast<T>(i)
+                    };
+                    if (res > division_check) {
                         return 0; // Overflow en la siguiente multiplicación
                         // Tipos de longitud finita
+                    } else {
+                        res *= static_cast<T>(i);
                     }
                 }
-                res *= static_cast<T>(i);
             }
             return res;
         }
@@ -178,14 +283,31 @@ namespace implementation_details {
     template<IsFactorialSupportedInteger T>
     constexpr T factorial_rt(const T k) {
         using std::numeric_limits;
-        static constexpr size_t limit = get_factorial_limit<T>();
-        if constexpr (is_signed_v<T>) {
+
+        static constexpr bool is_signed_integer{
+            sign_type_info<T>() == fact_type_info::signed_int
+        };
+
+        const bool is_negative{
+            check_negative_rt<T>(k) == fact_sign_info::negative
+        };
+
+        const bool is_overflow{
+            check_overflow_rt<T>(k) == fact_over_info::overflow
+        };
+
+        static constexpr bool is_finite_integer{
+            capacity_type_info<T>() == fact_capacity_info::finite
+        };
+
+
+        if constexpr (is_signed_integer) {
             // TIPOS SIGNADOS
             if (k < 0) {
                 return static_cast<T>(-1); // ARGUMENTO NEGATIVO
-            } else if constexpr (limit != 0) {
+            } else if constexpr (is_finite_integer) {
                 // TIPOS DE LONGITUD FINITA
-                if (k > limit) {
+                if (is_overflow) {
                     return static_cast<T>(0); // OVERFLOW TIPOS SIGNADOS FINITOS
                 } else {
                     // TIPOS DE LONGITUD FINITA
@@ -209,9 +331,9 @@ namespace implementation_details {
             }
         } else {
             // TIPOS NO SIGNADOS
-            if constexpr (limit != 0) {
+            if constexpr (is_finite_integer) {
                 // TIPOS NO SIGNADOS DE LONGITUD FINITA
-                if (k > limit) {
+                if (is_overflow) {
                     return static_cast<T>(0); // OVERFLOW TIPOS NO SIGNADOS FINITOS
                 } else {
                     // TIPOS NO SIGNADOS DE LONGITUD FINITA
@@ -239,33 +361,57 @@ namespace implementation_details {
     // Genera una tabla extendida de factoriales para tipos de cualquier tamaño en tiempo de compilación.
     template<IsFactorialSupportedInteger T, size_t Size>
     constexpr auto generate_extended_factorial_table() -> std::array<T, Size> {
-        return make_constexpr_array<T, Size>([](size_t n) {
-            return factorial_rt<T>(static_cast<T>(n));
+        return make_constexpr_array<T, Size>([](const size_t index) -> T {
+            if (index == 0) {
+                // Posición 0: -1 para tipos con signo, 0 para sin signo (ya que no pueden ser negativos).
+                if constexpr (is_signed_v<T>) {
+                    return static_cast<T>(-1);
+                } else {
+                    return static_cast<T>(0);
+                }
+            } else if (index == 1) {
+                // Posición 1: 0 para overflow.
+                return static_cast<T>(0);
+            } else {
+                // Posición n: (n-2)!
+                return factorial_rt<T>(static_cast<T>(index - 2));
+            }
         });
     }
 
     // Función auxiliar para obtener el factorial de una tabla estática
     template<IsFactorialSupportedInteger T>
     constexpr T factorial_from_table(T n) {
-        constexpr size_t limit = get_factorial_limit<T>();
-
+        // 1. Manejar el caso de error: número negativo (solo para tipos con signo).
         if constexpr (is_signed_v<T>) {
             if (n < 0) {
-                return static_cast<T>(-1); // Caso 1: Número negativo
+                return static_cast<T>(-1);
             }
-        } 
-        
-        if constexpr (limit != 0) { // Aplica solo a tipos de longitud finita
-            if (static_cast<size_t>(n) > limit) {
-                return static_cast<T>(0); // Caso 2: Desbordamiento
+        }
+
+        // 2. Comprobar si el valor está dentro de los límites de la tabla precalculada (0 a 170).
+        constexpr size_t table_limit = get_factorial_limit<uint1024_t>(); // Es 170
+        if (n <= table_limit) {
+            // El valor está en el rango de la tabla, la usamos.
+            constexpr size_t sz_table = table_limit + 1 + 2; // +2 para casos especiales
+            static constexpr std::array<T, sz_table> table{
+                generate_extended_factorial_table<T, sz_table>()
+            };
+
+            // Comprobar si 'n' excede el límite del *tipo de dato* actual.
+            constexpr size_t type_limit = get_factorial_limit<T>();
+            if constexpr (type_limit != 0) {
+                // Si el tipo tiene un límite
+                if (n > type_limit) {
+                    return table[1]; // Devolver el valor de overflow precalculado.
+                }
             }
-        } 
-        
-        if constexpr (limit != 0 && limit <= 170) { // Caso 3: El número es válido y está en la tabla
-            constexpr size_t sz_table = limit + 1;
-            static constexpr std::array<T, sz_table> table{generate_extended_factorial_table<T, sz_table>()};
-            return table[static_cast<size_t>(n)];
-        } else { // Caso 4: Tipos de precisión arbitraria (o que exceden la tabla precalculada)
+            
+            // El valor es válido y está en la tabla.
+            return table[static_cast<size_t>(n) + 2]; // n! está en la posición n+2
+        } else {
+            // 3. Si n > 170, la tabla no es suficiente. Recurrir al cálculo en tiempo de ejecución.
+            //    Esto es principalmente para tipos de precisión arbitraria como mp_int_t.
             return factorial_rt<T>(n);
         }
     } // end function factorial_from_table
